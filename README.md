2214a4cb4213319cf4adfa8bb579274e1c4dd6bd
asteven <agstevens09@gmail.com>
2019-11-23 17:17:36 -0600

    Added the add, subtract, multiply, and divide functions to script.  Also added the operate function that calls add, subtract, multiply, or divided when two numbers and the appropriate operators are called.  Made the README.md a bit more readable as well.

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-#Current Project: Calculator
+# Current Project: Calculator
 Making an on-screen Calculator.  Should include basic calculator functions like Add, Subtract, Muliply, and Divide.  
 
-##Skills Used
+## Skills Used
 *
---
+++ script.js
@@ -0,0 +1,37 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b){
+    return a / b;
+}
+
+function operate(first, second, operator) {
+    let result;
+    switch (operator) {
+        case '+':
+            result = add(first, second);
+            break;
+        case '-':
+            result = subtract(first, second);
+            break;
+        case '*':
+            result = multiply(first, second);
+            break;
+        case '/':
+            result = divide(first, second);
+            break;
+        default:
+            result = "That operator is not recongnized, please try again.";
+            break;
+    }
+    return result;
+}

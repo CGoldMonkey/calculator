c8a6c6b63d4d78b523741987666410f7c8145fb8
asteven <agstevens09@gmail.com>
2019-11-26 17:16:23 -0600

    Fixed the issue with the addition operator where it wasn't adding the values but concatanating them as strings.

--- script.js
+++ script.js
@@ -10,8 +10,8 @@ let currentNumber = "";
 
 buttons.forEach(button => {
     button.addEventListener('click', (e) => {
-  //      console.log(button);
         let input = button.textContent;
+        appendDisplay(button.textContent);
         
          //if operator then save the number and operator
         if(operators.test(input)){
@@ -21,66 +21,25 @@ buttons.forEach(button => {
             mathExpression.push({number: currentNumber});
             currentNumber = "";
 
-     //       console.table(mathExpression);
+           //call operate on the numbers: *and / operater, then + -
 
-            //call operate on the numbers: *and / operater, then + -
-        //    let sum = mathExpression
             while(mathExpression.some(checkMultiplyDivide)) {
-         /*   if(mathExpression.some(expression => {
-                return expression.operator === "x" || expression.operator === "÷"})) {
-                console.log("x or divide after =");
-            }   */   
-          //      console.log("x or divide after =");
-                //look for * or divide
-
-         /*       let multOrDivideIndex = mathExpression.findIndex(checkMultiplyDivide);
-                let first = mathExpression[multOrDivideIndex];
-                let second = mathExpression[multOrDivideIndex+1];
-                let sumElements = operate(first.number, first.operator, second.number);
-                console.log(sumElements);
-
-                //assign the updated sum to the one to the right of it
-                second.number = sumElements;
-                console.log(mathExpression[multOrDivideIndex+1].number);
-
-                //delete the current object// or make new array
-                mathExpression = [...mathExpression.slice(0, multOrDivideIndex),
-                        ...mathExpression.slice(multOrDivideIndex+1)];
-                console.table(mathExpression);
-                */
                let multOrDivideIndex = mathExpression.findIndex(checkMultiplyDivide);
                let sum = sumOfNumbersToRight(multOrDivideIndex);
                updateMathExpression(sum, multOrDivideIndex);
-
             }
 
-            //if ther isn't any multiply or divide
-          //  let currentIndex = 0;
-
-            while(mathExpression.length !== 1) {
-                let currentIndex = 0;
-                let curFirst = mathExpression[currentIndex];
-                let curSecond = mathExpression[currentIndex + 1];
-                let curSum = operate(curFirst.number, curFirst.operator, curSecond.number);
-
-                curSecond.number = curSum;
-
-                mathExpression = [...mathExpression.slice(0, currentIndex),
-                    ...mathExpression.slice(currentIndex+1)];
-
-                    console.table(mathExpression);
-
-                curSum = 0;
+            while(mathExpression.length > 1) {
+                let beginningIndex = 0;
+                let sum = sumOfNumbersToRight(beginningIndex);
+                updateMathExpression(sum, beginningIndex);
             }
-
             //dispay final number
             appendDisplay(mathExpression[0].number)
 
         } else {
             currentNumber += input;
         }
-        appendDisplay(button.textContent);
-  //      console.log("currentNumber: "+currentNumber);
         console.table(mathExpression);
 
     })
@@ -92,24 +51,6 @@ function sumOfNumbersToRight (index) {
     let sum = operate(current.number, current.operator, second.number);
 
     return sum;
-
- /*   let multOrDivideIndex = mathExpression.findIndex(checkMultiplyDivide);
-                let first = mathExpression[multOrDivideIndex];
-                let second = mathExpression[multOrDivideIndex+1];
-                let sumElements = operate(first.number, first.operator, second.number);
-                console.log(sumElements);
-
-                //assign the updated sum to the one to the right of it
-                second.number = sumElements;
-                console.log(mathExpression[multOrDivideIndex+1].number);
-
-                //delete the current object// or make new array
-                mathExpression = [...mathExpression.slice(0, multOrDivideIndex),
-                        ...mathExpression.slice(multOrDivideIndex+1)];
-                console.table(mathExpression);
-                */
-            
-
 }
 
 function updateMathExpression(newNumber, currentIndex) {
@@ -146,7 +87,7 @@ function checkForOperator(input){
 //let displayValue = 
 
 function add(a, b) {
-    return a + b;
+    return parseInt(a) + parseInt(b);
 }
 
 function subtract(a, b) {

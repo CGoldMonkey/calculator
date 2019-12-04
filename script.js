2f41c825332e8ce3816eb7e7b0b1fe98ea93026d
asteven <agstevens09@gmail.com>
2019-12-04 17:17:14 -0600

    Deleted some unnecessary variables and comments to make things clearer

--- script.js
+++ script.js
@@ -16,11 +16,9 @@ buttons.forEach(button => {
 
         console.log("Beginning CurNum: " + currentNumber);
 
-
         if (input !== '⌫') {
             appendDisplay(input);
         }
-
         //if operator then save the number and operator
         if (operators.test(input)) {
             mathExpression.push({ number: currentNumber, operator: input });
@@ -29,7 +27,6 @@ buttons.forEach(button => {
         } else if (input === "CLEAR") {
             clearDisplay();
         } else if (input === "⌫") {
-            
             //CHANGE EXPRESSION
             if(currentNumber.length > 0) { //if current number isn't empty then bacspace current number
                 currentNumber = currentNumber.slice(0, currentNumber.length - 1);
@@ -41,22 +38,17 @@ buttons.forEach(button => {
                 currentNumber = mathExpression[lastIndex].number;
                 mathExpression.pop();
                 console.table(mathExpression);
-
-                //check num and subtract by one//i.e set the current num
-
-                //set the current num, delete last array
-
             } 
             //CHANGE DISPLAY
             backspaceDisplay();
+            checkForDecimal(currentNumber.toString());
 
         } else if (input === "=") {
             mathExpression.push({ number: currentNumber });
-            //    currentNumber = "";
+
             //call operate on the numbers: *and / operater, then + -
             evaluateExpression: {
                 while (mathExpression.some(checkMultiplyDivide)) {
-                    //                  let divideByZero = false;
                     let multOrDivideIndex = mathExpression.findIndex(checkMultiplyDivide);
                     let sum = sumOfNumbersToRight(multOrDivideIndex);
                     if (divideByZero === true) {
@@ -73,26 +65,15 @@ buttons.forEach(button => {
                 }
                 //dispay final number
                 mathExpression[0].number = roundToThousandths(mathExpression[0].number);
-                let finalNumber = mathExpression[0].number;
-                //appendDisplay(finalNumber)
-                display.textContent = finalNumber;
-                currentNumber = finalNumber;
+                currentNumber = mathExpression[0].number;
+                display.textContent = currentNumber;
                 checkForDecimal(currentNumber.toString());
                 mathExpression = [];
             }
-
         } else { //done here so that the operators or the = string aren't added to the number
             currentNumber += input;
             console.log("Current Num: " + currentNumber);
             checkForDecimal(currentNumber);
-
-            //problems with starting off with a decimal point
-            //probably better to make function of our own
-            /*        if(currentNumber.includes(".")){
-                        decimalButton.disabled = true;
-                    } else {
-                        decimalButton.disabled = false;
-                    }*/
         }
         console.table(mathExpression);
     })
@@ -132,7 +113,6 @@ function sumOfNumbersToRight(index) {
     let current = mathExpression[index];
     let second = mathExpression[index + 1];
     if (current.operator == '÷' && second.number == 0) {
-        //    clearDisplay();
         divisionByZero();
     }
     let sum = operate(current.number, current.operator, second.number);
